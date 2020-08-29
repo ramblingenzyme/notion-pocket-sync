9d5a3caebc09743b6117fdd7125e04260115a7bd
Satvik Sharma <satvik.sharma2@gmail.com>
2020-08-29 13:36:25 +1000

    It works, kinda

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import "dotenv/config";
+// @ts-ignore
+import Notabase from "@vihanb/notabase";
+
+const nb = new Notabase({
+    token: process.env.NOTION_AUTH,
+})
+
+async function main() {
+    const readingList = await nb.fetch(process.env.READING_LIST_PAGE);
+    console.log(JSON.stringify(readingList.rows, null, 2));
+}
+
+main();
